fe8f50ae2f8a853951e62d31472acf88443ff933
antoshkaxxr <gbanton2004@yandex.ru>
2024-10-09 15:20:42 +0500

    modal window for question

--- src/pages/form-builder-page/form-builder-page.tsx
+++ src/pages/form-builder-page/form-builder-page.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import QuestionTypeModal from '../../components/parts-of-builder-survey/question-type-modal/question-type-modal';
+import QuestionInputModal from '../../components/parts-of-builder-survey/question-input-modal/question-input-modal';
 import './custom-builder.css';
 
 const FormBuilderPage: React.FC = () => {
-    const [isModalOpen, setModalOpen] = useState<boolean>(false);
-    const [_, setSelectedQuestionType] = useState<string | null>(null);
+    const [isTypeModalOpen, setTypeModalOpen] = useState<boolean>(false);
+    const [isInputModalOpen, setInputModalOpen] = useState<boolean>(false);
+    const [selectedQuestionType, setSelectedQuestionType] = useState<string | null>(null);
 
     const handleSelectQuestionType = (type: string) => {
         setSelectedQuestionType(type);
         console.log('Выбранный тип вопроса:', type); // Логика для обработки выбранного вопроса
+        setInputModalOpen(true); // Открываем модальное окно для ввода вопроса
+        setTypeModalOpen(false); // Закрываем модальное окно выбора типа
+    };
+
+    const handleSubmitQuestion = (question: string, options?: string[]) => {
+        console.log('Сохранённый вопрос:', question, options);
+        // Здесь вы можете добавить логику для сохранения вопроса в survey
     };
 
     const handleSubmit = async () => {
@@ -43,12 +52,18 @@ const FormBuilderPage: React.FC = () => {
         <div className={'builder-survey'}>
             <h1>Конструктор форм</h1>
             <p>Введите название формы:</p>
-            <button className={'adder'} onClick={() => setModalOpen(true)}></button>
+            <button className={'adder'} onClick={() => setTypeModalOpen(true)}>Добавить вопрос</button>
             <button className={'submit'} onClick={handleSubmit}>Отправить опрос</button>
             <QuestionTypeModal
-                isOpen={isModalOpen}
-                onClose={() => setModalOpen(false)}
+                isOpen={isTypeModalOpen}
+                onClose={() => setTypeModalOpen(false)}
                 onSelect={handleSelectQuestionType}
+            />
+            <QuestionInputModal
+                isOpen={isInputModalOpen}
+                onClose={() => setInputModalOpen(false)}
+                questionType={selectedQuestionType}
+                onSubmit={handleSubmitQuestion}
             />
         </div>
     );
