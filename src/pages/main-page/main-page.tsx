9b810157891bc48a6fe0a022e96c58e9652ec792
lehakurshev <lehakurshev@mail.ru>
2024-10-12 18:24:20 +0500

    Add canvas in main page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,10 +1,19 @@
+import './styles.css';
+import './index.js';
+
 function MainPage() {
     return (
         <>
+            
+            
+            <canvas id="canvas"></canvas>
             <h1>Welcome!</h1>
             <h2>This is a main page</h2>
+
         </>
     );
+    
 }
+
 
 export default MainPage;
